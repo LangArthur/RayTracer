1dbfdc031c6e4e9203dc0bcde618a40564b8c9c9
alang <arthur.lang@epitech.eu>
2020-04-08 19:05:13 +0200

    add ray object, Point2D and Point3D

---
+++ Arthur/src/utils/Point2D.hpp
@@ -0,0 +1,43 @@
+/*
+ * Created on Wed Apr 08 2020
+ *
+ * Copyright (c) RayTracer 2020 Arthur Lang
+ * Point2D.hpp
+ */
+
+namespace math {
+
+    template<typename T>
+
+    class Point2D {
+
+        public:
+            Point2D(T x, T y) : _x(x), _y(y) {};
+            ~Point2D();
+
+            inline const T x() const { return _x; };
+            inline const T y() const { return _y; };
+
+            Point2D operator+(const Point2D& other)
+            {
+                Point2D<T> res(_x + other.x(), _y + other.y());
+                return res;
+            }
+
+            Point2D operator-(const Point2D& other)
+            {
+                Point2D<T> res(_x - other.x(), _y - other.y());
+                return res;
+            }
+
+            Point2D operator*(const Point2D& other)
+            {
+                Point2D<T> res(_x * other.x(), _y * other.y());
+                return res;
+            }
+
+        private:
+            T _x;
+            T _y;
+        };
+}
---
+++ Arthur/src/utils/Point3D.hpp
@@ -0,0 +1,45 @@
+/*
+ * Created on Wed Apr 08 2020
+ *
+ * Copyright (c) Raytracer 2020 Arthur Lang
+ * Point3D.hpp
+ */
+
+namespace math {
+
+    template<typename T>
+
+    class Point3D {
+
+        public:
+            Point3D(T x, T y, T z) : _x(x), _y(y), _z(z) {};
+            ~Point3D() {};
+
+            inline const T x() const { return _x; };
+            inline const T y() const { return _y; };
+            inline const T z() const { return _z; };
+
+            Point3D operator+(const Point3D& other)
+            {
+                Point3D<T> res(_x + other.x(), _y + other.y(), _z + other.z());
+                return res;
+            }
+
+            Point3D operator-(const Point3D& other)
+            {
+                Point3D<T> res(_x - other.x(), _y - other.y(), _z - other.z());
+                return res;
+            }
+
+            Point3D operator*(const Point3D& other)
+            {
+                Point3D<T> res(_x * other.x(), _y * other.y(), _z * other.z());
+                return res;
+            }
+
+        private:
+            T _x;
+            T _y;
+            T _z;
+        };
+}
