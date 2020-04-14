c095682acf64d51b993b4181eae00bfefc8a9de4
Lucas <lucas.tabis@epitech.eu>
2020-04-14 10:53:17 +0200

    :arrow_up: Retreived some useless ray computing.

--- Rust/ray-tracer/src/props/ray.rs
+++ Rust/ray-tracer/src/props/ray.rs
@@ -45,24 +45,26 @@ impl Ray {
     pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
 
         // We first have to look for the aspect ratio of the image.
-        let aspect_ratio = if scene.width > scene.height {
-            scene.width as f64 / scene.height as f64
-        } else {
-            1.0
-        };
+        // let aspect_ratio = if scene.width > scene.height {
+        //     scene.width as f64 / scene.height as f64
+        // } else {
+        //     1.0
+        // };
 
         // We stil need the fov slide, but I don't know how to do it yet.
         // let fov = ??;
 
-        // We add .5 to put the ray in the center of a pixel, not at it's (0, 0) coordinates.
+        // Calculating x and y coordinates as the [-1; 1] plan coordinates.
         // Mul by 2 and sub by 1 transforms the result in the [-1; 1] bounds.
-        let image_plane_x = (((x as f64 * 0.5) / scene.width  as f64) * 2.0 - 1.0) * aspect_ratio;
-        let image_plane_y = ((y as f64 * 0.5) / scene.height as f64) * 2.0;
+        // For the y plane coordinates, where calculating the inverse because
+        // positive y are going downwards.
+        let image_plane_x =  ((x as f64) / scene.width  as f64) * 2.0 - 1.0;
+        let image_plane_y = -((y as f64) / scene.height as f64) * 2.0 + 1.0;
 
         Ray {
             origin:    Vector3::new(0.0, 0.0, 0.0),
             // Normalizing the vector ([1, 1, 1] unit vector).
-            // z: -1 is because the plane faces the camera.
+            // z: -1 is because the plane faces the camera and is -1.0 unit in front of it.
             direction: Vector3::new(image_plane_x, image_plane_y, -1.0).normalize()
         }
     }
