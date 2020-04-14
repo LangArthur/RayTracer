373ddf5b114d7fe5a95cfeb7a0cc27544f4343fa
Lucas <lucas.tabis@epitech.eu>
2020-04-14 09:57:32 +0200

    :pencil: Added some documentation + minor fixes.

--- Rust/ray-tracer/src/props/ray.rs
+++ Rust/ray-tracer/src/props/ray.rs
@@ -15,6 +15,7 @@ pub struct Ray {
 
 impl Ray {
 
+    /// Creating a ray with an origin.
     pub fn new(x: f64, y: f64, z: f64) -> Ray {
         Ray {
             origin:    Vector3::new(x, y, z),
@@ -22,6 +23,7 @@ impl Ray {
         }
     }
 
+    /// Creating an empty ray at zero coordinates.
     pub fn zero() -> Ray {
         Ray {
             origin:    Vector3 { x: 0.0, y: 0.0, z: 0.0 },
@@ -40,7 +42,7 @@ impl Ray {
     ///   -1
     ///
     /// We need to transform pixels into the scene's coordinates, that's why we use an "image plane".
-    pub fn create_prime(x: f64, y: f64, scene: &Scene) -> Ray {
+    pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
 
         // We first have to look for the aspect ratio of the image.
         let aspect_ratio = if scene.width > scene.height {
@@ -50,17 +52,17 @@ impl Ray {
         };
 
         // We stil need the fov slide, but I don't know how to do it yet.
-        let fov_adj = (scene.fov.to_radians() / 2.0).tan();
-
+        // let fov = ??;
 
         // We add .5 to put the ray in the center of a pixel, not at it's (0, 0) coordinates.
         // Mul by 2 and sub by 1 transforms the result in the [-1; 1] bounds.
-        let image_plane_x = (((x * 0.5) / scene.width  as f64) * 2.0 - 1.0) * aspect_ratio * fov_adj;
-        let image_plane_y =  1.0 - ((y * 0.5) / scene.height as f64) * 2.0 * fov_adj;
+        let image_plane_x = (((x as f64 * 0.5) / scene.width  as f64) * 2.0 - 1.0) * aspect_ratio;
+        let image_plane_y = ((y as f64 * 0.5) / scene.height as f64) * 2.0;
 
         Ray {
             origin:    Vector3::new(0.0, 0.0, 0.0),
-            // Normalizing the vector ([1, 1, 1] unit vector). z: -1 is because the plane faces the camera.
+            // Normalizing the vector ([1, 1, 1] unit vector).
+            // z: -1 is because the plane faces the camera.
             direction: Vector3::new(image_plane_x, image_plane_y, -1.0).normalize()
         }
     }
--- Rust/ray-tracer/tests/rendering_tests.rs
+++ Rust/ray-tracer/tests/rendering_tests.rs
@@ -9,15 +9,20 @@ use image;
 use image::GenericImageView;
 use image::GenericImage;
 
+/// Takes a scene in parameter and render it to an image with the image crate.
 pub fn render(scene: &rendering::scene::Scene) -> image::DynamicImage {
 
     let mut image = image::DynamicImage::new_rgb8(scene.width, scene.height);
 
+    // Iterating trough the pixels of the image.
     for x in 0..scene.width {
         for y in 0..scene.height {
 
-            let ray = props::ray::Ray::create_prime(x as f64, y as f64, scene);
+            // Tracing a ray.
+            let ray = props::ray::Ray::create_prime(x, y, scene);
 
+            // Checking if the ray has hit a sphere.
+            // (We will add more shapes in the futur)
             if scene.sphere.hit(&ray) {
                 image.put_pixel(x, y, image::Rgba([
                     scene.sphere.color.r,
@@ -41,7 +46,7 @@ fn test_can_render_scene() {
         height: 1000,
         fov: 90.0,
         sphere: rendering::sphere::Sphere {
-            center: Vector3::new(-2.5, 2.5, -5.0),
+            center: Vector3::new(0.0, 0.0, -5.0),
             radius: 1.0,
             color: props::color::Color {
                 r: 255,
@@ -53,7 +58,7 @@ fn test_can_render_scene() {
     };
 
     let img: image::DynamicImage = render(&scene);
-    img.save("fuckoff.png").unwrap();
+    img.save("test.png").unwrap();
     assert_eq!(scene.width, img.width());
     assert_eq!(scene.height, img.height());
 }
