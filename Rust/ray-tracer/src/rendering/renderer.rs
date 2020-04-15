c7ee9e2aa824d1e7c6abc93a03b9ae25edf293e7
Lucas <lucas.tabis@epitech.eu>
2020-04-15 09:31:27 +0200

    :zap: Optimized the renderer.

--- Rust/ray-tracer/src/rendering/renderer.rs
+++ Rust/ray-tracer/src/rendering/renderer.rs
@@ -56,11 +56,13 @@ fn render_pixel_on_image(x: u32, y: u32, scene: &rendering::scene::Scene, image:
     match obj_to_render {
         Some(obj_to_render) => {
 
+            let color_to_render = obj_to_render.color();
+
             image.put_pixel(x, y, image::Rgba([
-                obj_to_render.color().r,
-                obj_to_render.color().g,
-                obj_to_render.color().b,
-                obj_to_render.color().a
+                color_to_render.r,
+                color_to_render.g,
+                color_to_render.b,
+                color_to_render.a
             ]));
         },
 
